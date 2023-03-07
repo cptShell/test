6860586280808e2420fe234f2602cd9647e0e912
cptShell <repozheka@gmail.com>
2023-03-07 16:39:45 +0300

    feat: add css variables, landing for SignUp component

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { SignUp } from "./pages/signup/SignUp";
-import "./index.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
