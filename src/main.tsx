9bdc8280683b0f74bb1affc645840fcb9f0cd938
cptShell <repozheka@gmail.com>
2023-03-07 16:00:38 +0300

    feat: add basic file structure, add reset styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SignUp } from "./pages/signup/SignUp";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<SignUp />
+	</React.StrictMode>
+);
